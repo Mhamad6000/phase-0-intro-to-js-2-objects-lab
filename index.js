270503605a0410f70681ee2d3880b12856ffd8b3
Mhamad6000 <mhamadothman6000@gmail.com>
2022-10-31 19:09:26 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+const employee = { name: "mhamad", streetAddress: "mho" };
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  const newEmp = { ...employee };
+  newEmp[key] = value;
+  return newEmp;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value;
+  return employee;
+}
+function deleteFromEmployeeByKey(employee, key) {
+  const newEmp = { ...employee };
+  newEmp[key] = undefined;
+  return newEmp;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  employee[key] = undefined;
+  return employee;
+}
